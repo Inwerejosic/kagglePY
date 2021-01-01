016d68ec522ad86d19c0982990bd941917d2bc81
Inweregbu Joseph Chukwuka <inwerejosic@protonmail.com>
2021-01-01 20:11:39 +0100

    Wishing you all a glorious new year!

---
+++ kg_assign_3.py
@@ -0,0 +1,21 @@
+# without the python datetime 
+def wishes(new_year):
+    if(new_year == 2021):
+        print("Happy New Year, 2021")
+wishes(2021)
+print("From Inwerejosic with Love!")  
+
+# datetime module imported
+
+from datetime import datetime as dt
+
+
+def greetings(verify):
+    if verify.month == 1:
+        return (f"Happy new year, welcome to {verify.year}")
+    else:
+        return(f"We still dey {verify.year}, wait small for the year")
+
+date_check = dt.now()
+
+print(greetings(date_check))
