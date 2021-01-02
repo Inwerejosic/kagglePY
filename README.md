4abc2d6c0498e5af41df7001a1fa2753a66a581b
Inweregbu Joseph Chukwuka <inwerejosic@protonmail.com>
2021-01-02 14:37:24 +0100

    We are on lists now

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # kagglePY
 
 I am starting my python journey afresh and with a lot of zest, Data Science I am conquering you this year.
+
+it been over 40 hours and I won't give up on myself ever again.
---
+++ ks_lists.py
@@ -0,0 +1,19 @@
+primes = [2, 3, 5, 7]
+print(primes[3])
+
+print(9 in primes)
+
+
+planets = ['Mecury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Pluto']
+
+print('Mars' in planets)
+
+print(planets[3])
+
+print(planets[0:5])
+
+print(planets.pop())
+
+print(planets.index ('Saturn'))
+
+planets(help)
